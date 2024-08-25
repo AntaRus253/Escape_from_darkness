4ebcac19c703b7319c058d15bc0a5cc0487b465f
Ytkin Roman <mc3heisenberg@gmail.com>
2024-08-25 22:17:19 +0500

    Upd player script and destructible platform

--- Assets/Scripts/Movement/Controller.cs
+++ Assets/Scripts/Movement/Controller.cs
@@ -130,4 +130,13 @@ public class Controller : MonoBehaviour
     {
         readyToJump = true;
     }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit) {
+        if (hit.gameObject.CompareTag("DestructiblePlatform")) {
+            DestructiblePlatform platform = hit.gameObject.GetComponent<DestructiblePlatform>();
+            if (platform != null) {
+                platform.StartDestruction();
+            }
+        }
+    }
 }
--- Assets/Scripts/Platforms/DestructiblePlatform.cs
+++ Assets/Scripts/Platforms/DestructiblePlatform.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
 using System.Collections;
 
-public class DestructiblePlatform : MonoBehaviour
-{
+public class DestructiblePlatform : MonoBehaviour {
     public float destructionDelay = 2.1f;
     private bool isPlayerInContact = false;
-    // public GameObject playerObject;
 
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("Player") && !isPlayerInContact) {
+    public void StartDestruction() {
+        if (!isPlayerInContact) {
             isPlayerInContact = true;
             StartCoroutine(Destroy());
         }
