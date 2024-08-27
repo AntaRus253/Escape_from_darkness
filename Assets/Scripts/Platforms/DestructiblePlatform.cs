b233b76ab08c15ce23e9b2244ce48cf0bc3d87b3
Ytkin Roman <mc3heisenberg@gmail.com>
2024-08-27 23:58:01 +0500

    Add sound distrack platforms

--- Assets/Scripts/Platforms/DestructiblePlatform.cs
+++ Assets/Scripts/Platforms/DestructiblePlatform.cs
@@ -3,17 +3,48 @@ using System.Collections;
 
 public class DestructiblePlatform : MonoBehaviour {
     public float destructionDelay = 2.1f;
+    public float additionalDelayAfterSound = 5f;
     private bool isPlayerInContact = false;
+
+    public AudioClip destructionSound;
+    private AudioSource audioSource;
+
+    private MeshRenderer meshRenderer;
+    private Collider platformCollider;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+        meshRenderer = GetComponent<MeshRenderer>();
+        platformCollider = GetComponent<Collider>();
+    }
 
     public void StartDestruction() {
         if (!isPlayerInContact) {
             isPlayerInContact = true;
-            StartCoroutine(Destroy());
+            StartCoroutine(DestroyPlatform());
         }
     }
 
-    private IEnumerator Destroy() {
+    private IEnumerator DestroyPlatform() {
         yield return new WaitForSeconds(destructionDelay);
+        PlayDestructionSound();
+        HidePlatform();
+        yield return new WaitForSeconds(additionalDelayAfterSound);
         Destroy(gameObject);
+    }
+
+    private void PlayDestructionSound() {
+        if (destructionSound != null && audioSource != null) {
+            audioSource.PlayOneShot(destructionSound);
+        }
+    }
+
+    private void HidePlatform() {
+        if (meshRenderer != null) {
+            meshRenderer.enabled = false;
+        }
+        if (platformCollider != null) {
+            platformCollider.enabled = false;
+        }
     }
 }
