ba7ab127f0cd525c9345bff7c207ebf3ee98fd18
Anta <gus05522@gmail.com>
2024-08-27 23:49:29 +0300

    добавлен 2 уровень

--- Assets/Scripts/Platforms/DeadZone.cs
+++ Assets/Scripts/Platforms/DeadZone.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DeadZone : MonoBehaviour {
-    public int sceneToLoad = 1;
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
-            SceneManager.LoadScene(sceneToLoad);
+            SceneManager.LoadScene(PlayerPrefs.GetInt("MaxLevelComplite"));
         }
     }
 }
