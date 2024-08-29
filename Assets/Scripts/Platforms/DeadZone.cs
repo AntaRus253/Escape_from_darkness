971de5b5c5d231af2ac419e4eb9d7f61d13ff9f9
Ytkin Roman <mc3heisenberg@gmail.com>
2024-08-29 11:28:24 +0500

    Add sound darkness and crash platforms

--- Assets/Scripts/Platforms/DeadZone.cs
+++ Assets/Scripts/Platforms/DeadZone.cs
@@ -4,8 +4,14 @@ using UnityEngine.SceneManagement;
 public class DeadZone : MonoBehaviour {
     public int sceneToLoad = 1;
 
+    void Start() {
+
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
+
+
             SceneManager.LoadScene(sceneToLoad);
         }
     }
